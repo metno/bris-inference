8b493243d480591fc8e3880bda927431c0a0803e
Thomas Nipen <tnipen@gmail.com>
2024-12-10 20:05:52 +0100

    Use a single class for Indices

--- bris/indices.py
+++ bris/indices.py
@@ -3,59 +3,9 @@ from functools import cached_property
 from checkpoint import Checkpoint
 
 
-class DataIndices:
-    def __init__(self, ckpt: Checkpoint):
-        self.ckpt = ckpt
-
-    @cached_property
-    def _data_indices_input(self) -> dict:
-        return self.ckpt.data_indices.input
-
-    @cached_property
-    def data_indices_full(self) -> dict:
-        return self.ckpt._data_indices_input.full
-
-    @cached_property
-    def data_indices_forcing(self) -> dict:
-        return self.ckpt._data_indices_input.forcing
-
-    @cached_property
-    def data_indices_diagnostic(self) -> dict:
-        return self.ckpt._data_indices_input.diagnostic
-
-    @cached_property
-    def data_indices_prognostic(self) -> dict:
-        return self.ckpt._data_indices_input.prognostic
-
-
-class ModelIndices:
-    def __init__(self, ckpt: Checkpoint):
-        self.ckpt = ckpt
-
-    @cached_property
-    def _model_indices_model(self) -> dict:
-        return self.ckpt.model_indices.input
-
-    @cached_property
-    def model_indices_full(self) -> dict:
-        return self.ckpt._model_indices_input.full
-
-    @cached_property
-    def model_indices_diagnostic(self) -> dict:
-        return self.ckpt._model_indices_input.diagnostic
-
-    @cached_property
-    def model_indices_prognostic(self) -> dict:
-        return self.ckpt._model_indices_input.prognostic
-
-    @cached_property
-    def model_indices_forcing(self) -> dict:
-        return self.ckpt._model_indices_input.forcing
-
-    @cached_property
-    def model_indices_diagnostic(self) -> dict:
-        return self.ckpt._model_indices_input.diagnostic
-
-    @cached_property
-    def model_indices_prognostic(self) -> dict:
-        return self.ckpt._model_indices_input.prognostic
+class Indices:
+    def __init__(self, full, forcing, diagnostic, prognostic):
+        self.full = full
+        self.forcing = forcing
+        self.diagnostic = diagnostic
+        self.prognostic = prognostic
