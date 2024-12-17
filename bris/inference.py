40a42aff2c88a4e5690f0a7c9ecb990bb86d50f9
arams <arams@met.no>
2024-12-17 23:00:59 +0100

    added some typing, input args and reordering of methods

--- bris/inference.py
+++ bris/inference.py
@@ -1,34 +1,47 @@
+from typing import Any, Optional
+
 import torch
 from functools import cached_property
 import pytorch_lightning as pl
 
+from anemoi.training.distributed.strategy import DDPGroupStrategy
+from anemoi.utils.config import DotDict
 
 from .checkpoint import Checkpoint
 from .writer import CustomWriter
+from .data.datamodule import DataModule
 
 
 class Inference:
     def __init__(
         self,
-        model,
-        datamodule,
-        callbacks,
-        num_gpus_per_node=1,
-        num_nodes=1,
-        precision=None,
-        device=None,
-        deterministic=True,
-    ):
+        config: DotDict,
+        model: pl.LightningModule,
+        callbacks: Any,
+        datamodule: DataModule,
+        checkpoint: Checkpoint,
+        precision: Optional[str] = None,
+        device: Optional[str] = None,
+    ) -> None:
 
+        self.config = config
+        self.model = model
         self.checkpoint = checkpoint
         self.callbacks = callbacks
         self.datamodule = datamodule
-        self.deterministic = deterministic
-
+        self.deterministic = self.config.deterministic
+        self.precision = precision
         self._device = device
 
-    def run(self):
-        self.trainer.predict(self.model, datamodule=self.datamodule, return_predictions=False)
+    @property
+    def device(self) -> str:
+        if self._device is None:
+            if torch.cuda.is_available() and torch.backends.cuda.is_built():
+                return "cuda"
+            else:
+                return "cpu"
+        else:
+            return self._device
 
     @cached_property
     def trainer(self) -> pl.Trainer:
@@ -42,19 +55,14 @@ class Inference:
             ),
             devices=self.config.hardware.num_gpus_per_node,
             num_nodes=self.config.hardware.num_nodes,
-            precision=self.config.training.precision,
+            precision=self.config.precision if not self.precision else self.precision,
             inference_mode=True,
             use_distributed_sampler=False,
             callbacks=self.callbacks,
         )
         return trainer
 
-    @property
-    def device(self) -> str:
-        if self._device is None:
-            if torch.cuda.is_available() and torch.backends.cuda.is_built():
-                return "cuda"
-            else:
-                return "cpu"
-        else:
-            return self._device
+    def run(self):
+        self.trainer.predict(
+            self.model, datamodule=self.datamodule, return_predictions=False
+        )
