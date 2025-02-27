2a28ea00a5e099905c40489887528db0dce04132
Olav Ersland <olave@met.no>
2025-02-27 14:43:04 +0100

    Add pytest for logit and probability output from verif.
    Added test_2() and corresponding netCDF-file.

--- tests/test_outputs_verif.py
+++ tests/test_outputs_verif.py
@@ -83,6 +83,32 @@ def test_1():
 
             output.finalize()
 
+
+def test_2():
+    """Test verif output with logits and probability variables."""
+    filename = os.path.dirname(os.path.abspath(__file__)) + "/files/verif_input_logits.nc"
+    sources = [VerifInput(filename)]
+
+    variables = ["u_800", "u_600", "2t", "v_500", "10u"]
+    lats = np.arange(50, 70)
+    lons = np.arange(5, 15)
+    leadtimes = np.arange(0, 3600 * 4, 3600)
+    num_members = 2
+    thresholds = [0.2, 0.5]
+    quantile_levels = [0.1, 0.9]
+
+    field_shape = [len(lats), len(lons)]
+
+    lats, lons = np.meshgrid(lats, lons)
+    lats = lats.flatten()
+    lons = lons.flatten()
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        ofilename = os.path.join(temp_dir, "otest.nc")
+        workdir = os.path.join(temp_dir, "verif_workdir")
+        altitudes = np.arange(len(lats))
+        pm = PredictMetadata(variables, lats, lons, altitudes, leadtimes, num_members, field_shape)
+        elev_gradient = 0
         # Test for logits:
         for max_distance in [None, 100000]:
             output = Verif(
@@ -101,5 +127,24 @@ def test_1():
 
             output.finalize()
 
+        # Test for threshold_probability:
+        for max_distance in [None, 100000]:
+            output = Verif(
+                predict_metadata=pm,
+                workdir=workdir,
+                filename=ofilename,
+                variable="te",
+                variable_type="threshold_probability",
+                obs_sources=sources,
+                units="probability",
+                thresholds=thresholds,
+                quantile_levels=quantile_levels,
+                elev_gradient=elev_gradient,
+                max_distance=max_distance,
+            )
+
+            output.finalize()
+
 if __name__ == "__main__":
     test_1()
+    test_2()
