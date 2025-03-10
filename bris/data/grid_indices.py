202fa0c798229b9a0d50c8578447e2a407b9fe43
Lars Falk-Petersen <larsfp@met.no>
2025-03-10 15:22:53 +0100

    Fix formatting and unneccesary else.

--- bris/data/grid_indices.py
+++ bris/data/grid_indices.py
@@ -48,10 +48,12 @@ class BaseGridIndices(ABC):
         return {}
 
     @abstractmethod
-    def compute_grid_size(self, graph: HeteroData) -> int: ...
+    def compute_grid_size(self, graph: HeteroData) -> int:
+        ...
 
     @abstractmethod
-    def get_shard_indices(self, reader_group_rank: int) -> ArrayIndex: ...
+    def get_shard_indices(self, reader_group_rank: int) -> ArrayIndex:
+        ...
 
 
 class FullGrid(BaseGridIndices):
@@ -60,10 +62,9 @@ class FullGrid(BaseGridIndices):
     def compute_grid_size(self, graph: HeteroData) -> int:
         if hasattr(graph[self.nodes_name], "num_nodes"):
             return graph[self.nodes_name].num_nodes
-        elif "coords" in graph[self.nodes_name]:
+        if "coords" in graph[self.nodes_name]:
             return graph[self.nodes_name]["coords"].shape[0]
-        else:
-            raise ValueError("Could not compute grid size in graph")
+        raise ValueError("Could not compute grid size in graph")
 
     def get_shard_indices(self, reader_group_rank: int) -> ArrayIndex:
         return self.split_seq_in_shards(reader_group_rank)
