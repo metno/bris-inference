61f3ba14e1b9f98d73a9f0121233f514cebd9f35
arams <arams@met.no>
2025-03-19 16:29:26 +0100

    ModelBlock fixed . Major changes included such as  works with custom DDP and FSDP for gpu parellelism, selecting which block to analyze, etc..

--- bris/misc/modelblocks.py
+++ bris/misc/modelblocks.py
@@ -1,8 +1,11 @@
+import numpy as np
 import torch
 import einops
+import pytorch_lightning as pl
 
 from pathlib import Path
 from typing import Optional
+from torch.distributed.distributed_c10d import ProcessGroup
 
 from bris.checkpoint import Checkpoint
 from bris.misc.shapes import get_shape_shards
@@ -10,34 +13,30 @@ from bris.model import get_variable_indices
 from bris.data.datamodule import DataModule
 
 
-class ModelBlocks(Checkpoint):
-    def __init__(self, path: str) -> None:
-        super().__init__(path)
-        # sketch works
+class ModelBlocks(pl.LightningModule):
+    def __init__(self, checkpoint: Checkpoint, which_block: str) -> None:
+        super().__init__()
 
-        """
-        example from terminal:
-        
-        >>> from bris.checkpoint import Checkpoint
-        >>> checkpoint = Checkpoint(path)
-        >>> proc = checkpoint._model_instance.model.processor
-        >>> proc.state_dict()["proc.0.blocks.7.lin_value.bias"]
-        tensor([ 0.0225, -0.0109,  0.0125,  ..., -0.0232,  0.0181,  0.0023])
-        >>> # this is the state_dict. Checking if this is not random. Fetching model params
-        >>> checkpoint._get_copy_model_params["model.processor.proc.0.blocks.7.lin_value.bias"]
-        Parameter containing:
-        tensor([ 0.0225, -0.0109,  0.0125,  ..., -0.0232,  0.0181,  0.0023],
-            requires_grad=True)
-        >>> checkpoint._get_copy_model_params["model.processor.proc.0.blocks.7.lin_value.bias"] ==proc.state_dict()["proc.0.blocks.7.lin_value.bias"]
-        tensor([True, True, True,  ..., True, True, True])
-        """
+        assert isinstance(which_block, str)
+        assert which_block in [
+            "encoder",
+            "processor",
+            "decoder",
+        ], f"Expecting which_block param to be encoder, processor or decoder. Got {which_block}"
 
+        self.which_block = which_block
+        torch.set_float32_matmul_precision("high")
+
+        self.model_comm_group = None
+        self.model_comm_group_id = 0
+        self.model_comm_group_rank = 0
+        self.model_comm_num_groups = 1
+
+        self.model = checkpoint.model
         self._graph_name_data = "data"
         self._graph_name_hidden = "hidden"
-        # fetch from checkpoint class
-        self._model = self._model_instance.model
 
-        self.data_indices = self._model.data_indices
+        self.data_indices = self.model.data_indices
 
         if hasattr(self.data_indices, "internal_model") and hasattr(
             self.data_indices, "internal_data"
@@ -48,23 +47,15 @@ class ModelBlocks(Checkpoint):
             self.internal_model = self.data_indices.model
             self.internal_data = self.data_indices.data
 
-        # if torch.cuda.is_available():
-        self._encoder = self._model.encoder.to(
-            "cuda" if torch.cuda.is_available() else "cpu"
-        )
-        self._processor = self._model.encoder.to(
-            "cuda" if torch.cuda.is_available() else "cpu"
-        )
-        self._decoder = self._model.encoder.to(
-            "cuda" if torch.cuda.is_available() else "cpu"
-        )
+        self._encoder = self.model.model.encoder
+        self._processor = self.model.model.processor
+        self._decoder = self.model.model.decoder
 
     def encoder(
         self, x: torch.Tensor, model_comm_group: Optional[int] = None
     ) -> tuple[torch.Tensor, torch.Tensor]:
 
-        self._preproces_data(batch=x)
-
+        self._preproces_data(batch=x, model_comm_group=model_comm_group)
         return self._encoder(
             (self.x_data_latent, self.x_hidden_latent),
             batch_size=self.batch_size,
@@ -78,7 +69,7 @@ class ModelBlocks(Checkpoint):
         with_skip_connection: Optional[bool] = True,
         model_comm_group: Optional[int] = None,
     ) -> torch.Tensor:
-        # TODO: write logic
+
         x_latent_proc = self._processor(
             x_latent,
             batch_size=self.batch_size,
@@ -93,13 +84,14 @@ class ModelBlocks(Checkpoint):
 
     def decoder(
         self,
-        x: torch.Tensor,
+        x_latent_proc: torch.Tensor,
+        x_data_latent: torch.Tensor,
         model_comm_group: Optional[int] = None,
         abstract=False,
     ) -> torch.Tensor:
         # TODO: write logic
         x_out = self._decoder(
-            (x, self.x_data_latent),
+            (x_latent_proc, x_data_latent),
             batch_size=self.batch_size,
             shard_shapes=(self.shard_shapes_hidden, self.shard_shapes_data),
             model_comm_group=model_comm_group,
@@ -140,6 +132,7 @@ class ModelBlocks(Checkpoint):
             batch : input data
 
         """
+        # batch = batch.to(self.device)
         batch = self.model.pre_processors(batch, in_place=True)
         x = batch[..., self.internal_data.input.full]
 
@@ -153,14 +146,14 @@ class ModelBlocks(Checkpoint):
                     x,
                     "batch time ensemble grid vars -> (batch ensemble grid) (time vars)",
                 ),
-                self._model.node_attributes(
+                self.model.model.node_attributes(
                     self._graph_name_data, batch_size=self.batch_size
                 ),
             ),
             dim=-1,  # feature dimension
         )
 
-        self.x_hidden_latent = self._model.node_attributes(
+        self.x_hidden_latent = self.model.model.node_attributes(
             self._graph_name_hidden, batch_size=self.batch_size
         )
 
@@ -170,3 +163,62 @@ class ModelBlocks(Checkpoint):
         self.shard_shapes_hidden = get_shape_shards(
             self.x_hidden_latent, 0, model_comm_group
         )
+
+    def set_model_comm_group(
+        self,
+        model_comm_group: ProcessGroup,
+        model_comm_group_id: int = None,
+        model_comm_group_rank: int = None,
+        model_comm_num_groups: int = None,
+        model_comm_group_size: int = None,
+    ) -> None:
+        self.model_comm_group = model_comm_group
+        self.model_comm_group_id = model_comm_group_id
+        self.model_comm_group_rank = model_comm_group_rank
+        self.model_comm_num_groups = model_comm_num_groups
+        self.model_comm_group_size = model_comm_group_size
+
+    def set_reader_groups(
+        self,
+        reader_groups: list[ProcessGroup],
+        reader_group_id: int,
+        reader_group_rank: int,
+        reader_group_size: int,
+    ) -> None:
+        self.reader_groups = reader_groups
+        self.reader_group_id = reader_group_id
+        self.reader_group_rank = reader_group_rank
+        self.reader_group_size = reader_group_size
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        if self.which_block == "encoder":
+            return self.encoder(x, self.model_comm_group)
+
+        if self.which_block == "processor":
+            x_data_latent, x_latent = self.encoder(
+                x=x, model_comm_group=self.model_comm_group
+            )
+            return self.processor(
+                x_latent=x_latent, model_comm_group=self.model_comm_group
+            )
+
+        if self.which_block == "decoder":
+            x_data_latent, x_latent = self.encoder(x, self.model_comm_group)
+            x_latent_proc = self.processor(
+                x_latent,
+                model_comm_group=self.model_comm_group,
+                with_skip_connection=True,
+            )
+            return self.decoder(
+                x_latent_proc=x_latent_proc,
+                x_data_latent=x_data_latent,
+                model_comm_group=self.model_comm_group,
+            )
+
+    @torch.inference_mode
+    def predict_step(
+        self, batch: tuple[torch.Tensor, np.datetime64], batch_idx: int
+    ) -> torch.Tensor:
+        x, _ = batch
+        return self(x)
