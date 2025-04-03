93cf4212aeafd41ea335c84d2823d28cd3fa04d5
Lars Falk-Petersen <larsfp@met.no>
2025-04-03 10:54:12 +0200

    Add ignore to pylint

---
+++ tests/test_cf.py
@@ -0,0 +1,12 @@
+import os
+
+import bris.conventions.cf as cf
+
+
+def test_get_attributes():
+    test = cf.get_attributes("air_pressure")
+    assert test["standard_name"] == "air_pressure"
+    assert test["units"] == "hPa"
+
+if __name__ == "__main__":
+    test_get_attributes()
