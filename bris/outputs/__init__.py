5e101437771e809b6bc3b75d53386f5a9a558015
evenmn <evenmn@fys.uio.no>
2025-04-09 22:21:58 +0300

    Fixed bug occurring when 'add_forecast' is called multiple times
    Signed-off-by: evenmn <evenmn@fys.uio.no>

--- bris/outputs/__init__.py
+++ bris/outputs/__init__.py
@@ -90,18 +90,24 @@ class Output:
             pred: 3D numpy array with dimensions (leadtime, location, variable)
         """
 
-        # Append extra variables to prediction
-        extra_pred = list()
         for name in self.extra_variables:
-            if name == "ws":
-                Ix = self.pm.variables.index("10u")
-                Iy = self.pm.variables.index("10v")
-                curr = np.sqrt(pred[..., [Ix]] ** 2 + pred[..., [Iy]] ** 2)
-                extra_pred += [curr]
-            else:
-                raise ValueError(f"No recipe to compute {name}")
+            if name not in self.pm.variables:
+                self.pm.variables.append(name)
 
-        pred = np.concatenate([pred] + extra_pred, axis=2)
+        # only do this once. For multiple members, intermediate calls this several times
+        if pred.shape[2] != len(self.pm.variables):
+            # Append extra variables to prediction
+            extra_pred = list()
+            for name in self.extra_variables:
+                if name == "ws":
+                    Ix = self.pm.variables.index("10u")
+                    Iy = self.pm.variables.index("10v")
+                    curr = np.sqrt(pred[..., [Ix]] ** 2 + pred[..., [Iy]] ** 2)
+                    extra_pred += [curr]
+                else:
+                    raise ValueError(f"No recipe to compute {name}")
+
+            pred = np.concatenate([pred] + extra_pred, axis=2)
 
         assert pred.shape[0] == self.pm.num_leadtimes
         assert pred.shape[1] == len(self.pm.lats)
