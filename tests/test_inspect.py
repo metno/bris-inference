5d003546ac9329941ec9ad71eb80238add15628f
Lars Falk-Petersen <larsfp@met.no>
2025-04-30 12:48:31 +0200

    Update expected vars for new checkpoint

--- tests/test_inspect.py
+++ tests/test_inspect.py
@@ -126,12 +126,8 @@ def test_get_required_variables():
     expected_multi = {
         0: ["10u", "10v", "z"],
         1: [
-            "skt",
-            "msl",
-            "cos_longitude",
-            "cos_latitude",
-            "sin_longitude",
-            "sin_latitude",
+            "tp",
+            "2t",
         ],
     }
     checkpoint_multi = bris.checkpoint.Checkpoint("tests/files/multiencdec.ckpt")
@@ -139,7 +135,7 @@ def test_get_required_variables():
     for dataset, required_variables in required_multi.items():
         for expected_variable in expected_multi[dataset]:
             assert expected_variable in required_variables, (
-                f"Expected variable {expected_variable} not in test-checkpoint {checkpoint_multi}."
+                f"Expected variable {expected_variable} was not found in test-checkpoint {checkpoint_multi}."
             )
 
 
