dc4dcc21e0132cb32af5b9ba58cbc7dc5941830c
Lars Falk-Petersen <larsfp@met.no>
2025-11-07 15:04:17 +0100

    Remove commented code, https://github.com/metno/bris-inference/issues/132\#issuecomment-3501626207 (#212)

--- bris/outputs/netcdf.py
+++ bris/outputs/netcdf.py
@@ -395,11 +395,6 @@ class Netcdf(Output):
         proj_attrs = {}
         if self.proj4_str is not None:
             proj_attrs = projections.get_proj_attributes(self.proj4_str)
-            # proj_attrs["grid_mapping_name"] = "lambert_conformal_conic"
-            # proj_attrs["standard_parallel"] = (63.3, 63.3)
-            # proj_attrs["longitude_of_central_meridian"] = 15.0
-            # proj_attrs["latitude_of_projection_origin"] = 63.3
-            # proj_attrs["earth_radius"] = 6371000.0
         self.ds[self.conv_name("projection")] = ([], 0, proj_attrs, {"dtype": "int32"})
         utils.LOGGER.debug(
             f"netcdf._set_coords_gridded_not_interpolated in {pytime.perf_counter() - t0:.1f}s"
