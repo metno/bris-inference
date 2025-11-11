847e7a9b58216f37acb69bdb859f927ca1054e60
Håvard Holeid Haugen <havardhh@met.no>
2025-11-11 14:57:18 +0100

    Fix

--- bris/__main__.py
+++ bris/__main__.py
@@ -52,10 +52,11 @@ def main(arg_list: list[str] | None = None):
     config.checkpoints.forecaster.timestep_seconds = frequency_to_seconds(
         config.checkpoints.forecaster.timestep
     )
-    config.checkpoints.interpolator.timestep_seconds = int(
-        config.checkpoints.forecaster.timestep_seconds
-        / (len(checkpoints["interpolator"].config.training.explicit_times.target) + 1)
-    )
+    if "interpolator" in checkpoints:
+        config.checkpoints.interpolator.timestep_seconds = int(
+            config.checkpoints.forecaster.timestep_seconds
+            / (len(checkpoints["interpolator"].config.training.explicit_times.target) + 1)
+        )
 
     num_members = config["hardware"].get("num_members", 1)
 
