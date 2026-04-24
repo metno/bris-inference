8dc9230ba5e0ccdf342c4eb037a259bbe1c8dd90
Håvard Homleid Haugen <havardhh@met.no>
2026-04-24 12:50:12 +0000

    fix remove multiencdec from init

--- bris/model/__init__.py
+++ bris/model/__init__.py
@@ -1,4 +1,3 @@
-# Make BrisPredictor and MultiEncDecPredictor importable from bris.model:
+# Make BrisPredictor and Interpolator importable from bris.model:
 from .brispredictor import BrisPredictor  # noqa
-from .multiencdecpredictor import MultiEncDecPredictor  # noqa
 from .interpolator import Interpolator
