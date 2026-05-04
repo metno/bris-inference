1aacdaffa88d99e25d5f6fab74b3e4dbf983a357
Håvard Homleid Haugen <havardhh@met.no>
2026-05-04 08:55:50 +0000

    Fix dim of data for preprocc

--- bris/model/brispredictor.py
+++ bris/model/brispredictor.py
@@ -160,6 +160,7 @@ class BrisPredictor(BasePredictor):
                 dtype=data.dtype,
                 device=data.device,
             )
+            data_input = data_input[None, None, None, ...] # Add bs, time, ens dims needed by imputer
             data_input[..., self.indices[ds]["prognostic_input"]] = data[
                 ..., self.indices[ds]["prognostic_dataset"]
             ]
